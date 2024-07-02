e42768c9bfc192e2167019a2c948cf31cfe4207b
Thilina Lakshan <127662816+Th1l1na@users.noreply.github.com>
2024-07-02 19:50:22 +0530

    commit tailwind setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React, {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 import { styles } from '../styles'
 import { navLinks } from '../constants'
-import { Logo, menu, close } from '../assets'
+import{ logo, menu, close } from '../assets'
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='flex items-center justify-between w-full mx-auto max-w-7xl'>
+        <Link to="/" className='flex items-center gap-2'
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+          }}>
+          <img src={logo} alt="logo" className='object-contain w-9 h-9'/>
+          <p className='text-whitr text-[18px] font-bokd cursor-pointer'>Thilina <span className='hidden sm:block'>| Career Portfolio</span></p>
+        </Link>
+      </div>
+
     </nav>
   )
 }
